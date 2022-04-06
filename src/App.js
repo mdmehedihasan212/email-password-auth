198d1e5bfba2ce6b53ec57d3f2aa9b1b4dc2a430
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-07 02:41:13 +0600

    add two input field and solved reloade

--- src/App.js
+++ src/App.js
@@ -5,10 +5,31 @@ import app from './firebase.init';
 const auth = getAuth(app);
 
 function App() {
+
+  const handleToEmail = (event) => {
+    console.log(event.target.value);
+  }
+
+  const handleToPassword = (event) => {
+    console.log(event.target.value);
+  }
+
+  const handleToSubmit = (event) => {
+    console.log('form submit');
+    event.preventDefault();
+  }
+
   return (
     <div className='app-container'>
       <h1>Start Email Password Authentication</h1>
-    </div>
+      <form onSubmit={handleToSubmit}>
+        <input onChange={handleToEmail} type="email" />
+        <br />
+        <input onChange={handleToPassword} type="password" />
+        <br />
+        <input type="submit" value="Sing in" />
+      </form>
+    </div >
   );
 }
 
