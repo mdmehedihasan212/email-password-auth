487177ad15a0fa8e59ad6fcedc992378ff7cd6c4
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-07 03:20:52 +0600

    add bootstrap form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { getAuth } from "firebase/auth";
 import app from './firebase.init';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const auth = getAuth(app);
 
@@ -20,15 +23,34 @@ function App() {
   }
 
   return (
-    <div className='app-container'>
-      <h1>Start Email Password Authentication</h1>
-      <form onSubmit={handleToSubmit}>
-        <input onChange={handleToEmail} type="email" />
+    <div>
+      {/* <form onSubmit={handleToSubmit}>
+        <input onBlur={handleToEmail} type="email" />
         <br />
-        <input onChange={handleToPassword} type="password" />
+        <input onBlur={handleToPassword} type="password" />
         <br />
         <input type="submit" value="Sing in" />
-      </form>
+      </form> */}
+      <div className="registration w-50 mx-auto mt-5">
+        <Form onSubmit={handleToSubmit}>
+          <h1 className='text-primary'>Please Register!!!</h1>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onBlur={handleToEmail} type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control onBlur={handleToPassword} type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div >
   );
 }
