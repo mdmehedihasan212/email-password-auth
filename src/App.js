c531ca150a44f7f955d45fc3b5c0c982be5f267c
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-07 05:22:51 +0600

    add regular expersion

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ const auth = getAuth(app);
 function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState('');
+
 
   const handleToEmail = (event) => {
     setEmail(event.target.value);
@@ -21,6 +24,22 @@ function App() {
   }
 
   const handleToSubmit = (event) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return;
+    }
+
+    if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
+      setError('Password should contain at lest one special character')
+      return;
+    }
+
+    setValidated(true);
+    setError('')
+
     createUserWithEmailAndPassword(auth, email, password)
       .then(result => {
         const user = result.user;
@@ -39,20 +58,36 @@ function App() {
         <input type="submit" value="Sing in" />
       </form> */}
       <div className="registration w-50 mx-auto mt-5">
-        <Form onSubmit={handleToSubmit}>
+        <Form noValidate validated={validated} onSubmit={handleToSubmit}>
           <h1 className='text-primary'>Please Register!!!</h1>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control onBlur={handleToEmail} type="email" placeholder="Enter email" />
+            <Form.Control onBlur={handleToEmail} type="email" placeholder="Enter email" required />
+            <Form.Control.Feedback type="invalid">
+              Please choose a Email.
+            </Form.Control.Feedback>
             <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
+              We'll never share your email and password with anyone else.
             </Form.Text>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control onBlur={handleToPassword} type="password" placeholder="Password" />
+            <Form.Control onBlur={handleToPassword} type="password" placeholder="Password" required />
+            <Form.Control.Feedback type="invalid">
+              Please choose a Password.
+            </Form.Control.Feedback>
           </Form.Group>
+
+          {/* <Form.Group className="mb-3">
+            <Form.Check
+              required
+              label="Agree to terms and conditions"
+              feedback="You must agree before submitting."
+              feedbackType="invalid"
+            />
+          </Form.Group> */}
+          <p className='text-danger'>{error}</p>
           <Button variant="primary" type="submit">
             Register
           </Button>
